56d52bc63db49e5d82fc1b7ffd768806c51b631b
Daniel Prada <danielspradah0@gmail.com>
2026-05-12 13:51:51 -0500

    Add auth middleware to routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { body } from 'express-validator';
 import { createCategory } from '../controllers/categoryController.js';
+import { requireLogin } from '../middleware/validateSession.js';
 
 const categoryRoutes = Router();
+
+categoryRoutes.use(requireLogin);
 
 categoryRoutes.post('/createCategory', [
     body('nombre').notEmpty().withMessage('El nombre es obligatorio')
