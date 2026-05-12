f76557205ddf8f4e072b561a93b1304d91eba4e2
Daniel Prada <danielspradah0@gmail.com>
2026-05-12 13:51:24 -0500

    updated from npm install

--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -1,9 +1,9 @@
-import { AppError } from "../errors/appError";
+import { AppError } from "../errors/appError.js";
 
 export const requireLogin = (req, res, next) => {
   if (!req.session.user) {
     const err = new AppError("No autorizado", 401);
-    return err;
+    throw err;
   }
   // User is logged in, proceed
   next();
