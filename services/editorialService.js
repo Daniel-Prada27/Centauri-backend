a7e2a936c16481c556907eba1eb07893af1f200c
Daniel Prada <danielspradah0@gmail.com>
2026-05-16 20:50:00 -0500

    Normalize editorial name on creation

--- services/editorialService.js
+++ services/editorialService.js
@@ -1,12 +1,15 @@
 import { prisma } from '../prisma.js';
 import { AppError }from '../errors/appError.js';
+import normalize from './normalizeText.js';
 
 export const createEditorial = async (data) => {
-    const {nombre} = data;
+    let {nombre} = data;
 
     if (!nombre) {
         throw new AppError('El nombre es obligatorio', 400);
     }
+
+    nombre = normalize(nombre);
 
     const editorial = await prisma.editoriales.create({
         data: {
